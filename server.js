3c1a6d274c21d85edeca1114775cbbd148cbf4cb
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-11-15 20:10:14 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/task-tracker'));
+app.use(express.static(__dirname + '/dist/comp229-front-end-group3'));
 
 app.get('/*', function(req,res) {
 
