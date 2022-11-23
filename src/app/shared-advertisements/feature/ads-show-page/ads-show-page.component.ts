52093cffc153fe667e6d04909c4d4b21f656bd5a
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-11-22 23:14:46 -0500

    Fixed for show page

--- src/app/shared-advertisements/feature/ads-show-page/ads-show-page.component.ts
+++ src/app/shared-advertisements/feature/ads-show-page/ads-show-page.component.ts
@@ -4,7 +4,7 @@ import { Store } from '@ngrx/store';
 import { Observable, Subscription, tap } from 'rxjs';
 import { selectRouteParams } from 'src/app/shared/store/router.selectors';
 import { Advertisement } from '../../data-access/models/advertisement.model';
-import { selectAdvertisement } from '../../data-access/store';
+import { selectAllAdvertisement } from '../../data-access/store';
 
 @Component({
   selector: 'app-ads-show-page',
@@ -14,6 +14,7 @@ import { selectAdvertisement } from '../../data-access/store';
 export class AdsShowPageComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private store: Store, private router: Router) {
+    this.advertisements$ = this.store.select(selectAllAdvertisement);
   }
   ngOnInit() {
     this.route.data.subscribe(
@@ -22,5 +23,7 @@ export class AdsShowPageComponent implements OnInit {
       }
     )
   }
-  selectedAdvert: Advertisement | undefined;
+  selectedAdvert!: Advertisement;
+
+  advertisements$ : Observable<Advertisement[]>;
 }
