e80a063b9a47a12b635b8d042b0bd86b3801a09c
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2022-12-11 19:10:36 -0500

    submission bug fixed

--- src/app/shared-advertisements/ui/ads-create-update-form/ads-create-update-form.component.ts
+++ src/app/shared-advertisements/ui/ads-create-update-form/ads-create-update-form.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Advertisement, AdvertRequiredProps } from '../../data-access/models/advertisement.model';
 import dateFormat, { masks } from "dateformat";
 import { Storage, ref, uploadBytesResumable, getDownloadURL } from '@angular/fire/storage';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Component({
   selector: 'app-ads-create-update-form',
   templateUrl: './ads-create-update-form.component.html',
@@ -68,6 +69,8 @@ export class AdsCreateUpdateFormComponent implements OnInit {
   disableSubmit = true;
   progessNumber: number = 0;
   isUploadingCommenced = false;
+
+  shouldSubmitBeDisabled$: BehaviorSubject<boolean> = new BehaviorSubject(true)
   onFileSelected(event: any) {
     this.file = event.target.files[0];
   }
@@ -78,14 +81,13 @@ export class AdsCreateUpdateFormComponent implements OnInit {
       (snapshot) => {
         this.progessNumber = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         this.isUploadingCommenced = true;
-        this.disableSubmit = false;
       },
       (error) => {
         console.log(error);
       },
       () => {
+        this.shouldSubmitBeDisabled$.next(false);
         getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-          this.disableSubmit = false;
           this.advertForm.get('description')?.get('image')?.setValue(url)
         })
       }
