6269f70f33b57efe77306df7feba175d36f706c2
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2024-10-23 22:35:25 -0400

    upgrade to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ import { environment } from '../environments/environment';
     StoreModule.forFeature(fromAdvertisementsState.FeatureKey, fromAdvertisementsState.reducers),
     EffectsModule.forRoot([AdvertisementApiEffects, AuthEffects]),
     StoreRouterConnectingModule.forRoot(),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production , connectInZone: true}),
     StoreRouterConnectingModule.forRoot(),
   ],
   providers: [
